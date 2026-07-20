311f2fe3fb2f86f276c59234cd115c9b5ef2cabc
IRTIDEATH <enderjhass@gmail.com>
2026-07-20 21:02:33 +0700

    feat: add scalar and forgot to put frontend url

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { cors } from 'hono/cors'
 import { auth } from './auth'
 import { env } from './lib/env'
+import { Scalar } from '@scalar/hono-api-reference'
 
 const app = new Hono()
 
@@ -13,6 +14,11 @@ app.use('/api/*', cors({
 app.on(['POST', 'GET'], '/api/auth/*', (c) => {
 	return auth.handler(c.req.raw)
 })
+
+app.get('/docs', Scalar({
+	pageTitle: 'IgniteLecture API',
+	url: '/api/auth/open-api/generate-schema',
+}))
 
 app.get('/', (c) => {
 	return c.text('Hello Hono!')
